c0743a7f3871d61046190f187c2fdfb1e6188bac
n3sh-source <gustavo.carakaki@gmail.com>
2022-03-18 16:12:42 -0300

    4 files modified, 4 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,24 +8,23 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: 'Gustavo Phone',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'Um telefone minimalista, sem softwares proprietários',
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
   },
   {
     id: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
-  }
+    description: '',
+  },
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
